947da513a7e546f2e86285d4bceda284c561c62c
Mateusz Paprocki <mattpap@gmail.com>
2014-06-23 16:15:10 +0200

    Display environments and packages

--- package_manager/views.py
+++ package_manager/views.py
@@ -2,8 +2,23 @@ from flask import render_template, jsonify, redirect, abort, request, url_for
 
 from . import app
 
+from conda import config
+from conda.api import get_index
+from conda.envs import Env, get_envs
+from conda.resolve import Resolve, MatchSpec
+
 @app.route('/')
 def index():
-    envs = []
+    from conda.api import get_index
+    from conda.resolve import Resolve, MatchSpec
+
+    envs = [Env()] + get_envs()
     pkgs = []
+
+    resolve = Resolve(get_index(use_cache=True))
+
+    for group in sorted(resolve.groups):
+        for pkg in resolve.get_pkgs(MatchSpec(group)):
+            pkgs.append(pkg)
+
     return render_template('index.html', envs=envs, pkgs=pkgs)
