b7dd46cbbe014968942457119db1a0aa49f63153
Mateusz Paprocki <mattpap@gmail.com>
2014-06-24 13:31:01 +0200

    Display only names and versions

--- package_manager/views.py
+++ package_manager/views.py
@@ -6,6 +6,7 @@ from conda import config
 from conda.api import get_index
 from conda.envs import Env, get_envs
 from conda.resolve import Resolve, MatchSpec
+from conda.install import linked
 
 @app.route('/')
 def index():
@@ -16,9 +17,17 @@ def index():
     pkgs = []
 
     resolve = Resolve(get_index(use_cache=True))
+    groups = []
 
-    for group in sorted(resolve.groups):
-        for pkg in resolve.get_pkgs(MatchSpec(group)):
-            pkgs.append(pkg)
+    linked_pkgs = [ dist.rsplit('-', 2) for dist in linked(config.default_prefix) ]
+    installed = { name: (version, build) for (name, version, build) in linked_pkgs }
 
-    return render_template('index.html', envs=envs, pkgs=pkgs)
+    for name in sorted(resolve.groups):
+        pkgs = resolve.get_pkgs(MatchSpec(name))
+        installed_version = installed.get(name)
+        if installed_version is not None:
+            installed_version = installed_version[0]
+        latest_version = pkgs[-1].version
+        groups.append((name, installed_version, latest_version, pkgs))
+
+    return render_template('index.html', envs=envs, groups=groups)
