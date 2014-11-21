f18c94cfec144f4d9573e704caf8c3e81bd1879e
Andrew M. Farrell <amfarrell@mit.edu>
2014-11-20 18:35:50 -0600

    add coffeescript compiling to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
 gulp.task('mocha', function() {
   return gulp.src(['test/*.js'], {read: false})
@@ -8,5 +9,14 @@ gulp.task('mocha', function() {
     .on('error', gutil.log);
 });
 gulp.task('watch-mocha', function() {
-  gulp.watch(['test/**', 'conda_ui/static/conda_ui/**', 'conda_ui/static/conda-js/conda.js'], ['mocha']);
+  gulp.watch(['test/**', 'conda_ui/static/conda_ui/*.js', 'conda_ui/static/conda-js/conda.js'], ['mocha']);
 });
+gulp.task('coffee', function() {
+  return gulp.src(['conda_ui/static/conda_ui/*.coffee',])
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./conda_ui/static/conda_ui/'))
+});
+gulp.task('watch-coffee', function() {
+  gulp.watch(['conda_ui/static/conda_ui/*.coffee'], ['coffee']);
+});
+gulp.task('watch', ['watch-mocha', 'watch-coffee'])
