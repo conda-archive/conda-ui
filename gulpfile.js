4bdbe328bf07601f896697e64ecb77dbbda563de
Andrew M. Farrell <amfarrell@mit.edu>
2014-11-21 19:43:45 -0600

    add gulp task to bundle coffeescript files with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var paths = {
+  'coffee': {
+    'src': './conda_ui/static/conda_ui/conda.coffee',
+    'dest_filename': 'application.js',
+    'dest_path':'./conda_ui/static/conda_ui/'
+  }
+}
 
 gulp.task('mocha', function() {
   return gulp.src(['test/*.js'], {read: false})
@@ -20,3 +29,15 @@ gulp.task('watch-coffee', function() {
   gulp.watch(['conda_ui/static/conda_ui/*.coffee'], ['coffee']);
 });
 gulp.task('watch', ['watch-mocha', 'watch-coffee'])
+
+gulp.task('scripts', function() {
+  browserify({
+    'entries': paths.coffee.src,
+    'extensions': ['.coffee',],
+    'paths': ['conda_ui/static/conda_ui',],
+  }).transform('coffeeify')
+  .bundle()
+  .pipe(source(paths.coffee.dest_filename))
+  .pipe(gulp.dest(paths.coffee.dest_path))
+
+});
